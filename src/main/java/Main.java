dbbd9b277688965a2ffb5de6628924727fe7978e
Asadhulla <asadullahussainshaik@Asadullas-MacBook-Air.local>
2026-06-17 01:49:03 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
+
+        Scanner sc = new Scanner(System.in);
+
         System.out.print("$ ");
+
+        String command = sc.nextLine();
+
+        System.out.println(command + ": command not found");
     }
 }
