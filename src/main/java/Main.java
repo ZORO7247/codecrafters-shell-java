d483ca5479f81a5a91db63c9d4b1ce0f85a281dc
Asadhulla <asadullahussainshaik@Asadullas-MacBook-Air.local>
2026-06-17 02:06:09 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,32 @@
-import java.util.Scanner;
+else if (command.startsWith("type ")) {
 
-public class Main {
-    public static void main(String[] args) throws Exception {
+    String cmd = command.substring(5);
 
-        Scanner sc = new Scanner(System.in);
+    if (cmd.equals("echo") ||
+        cmd.equals("exit") ||
+        cmd.equals("type")) {
 
-        while (true) {
+        System.out.println(cmd + " is a shell builtin");
+    } else {
 
-            System.out.print("$ ");
+        String path = System.getenv("PATH");
+        String[] directories = path.split(":");
 
-            String command = sc.nextLine();
+        boolean found = false;
 
-            if (command.equals("exit")) {
+        for (String dir : directories) {
+
+            File file = new File(dir, cmd);
+
+            if (file.exists() && file.canExecute()) {
+                System.out.println(cmd + " is " + file.getAbsolutePath());
+                found = true;
                 break;
             }
+        }
 
-            if (command.startsWith("echo ")) {
-                System.out.println(command.substring(5));
-            }
-            else if (command.startsWith("type ")) {
-
-                String cmd = command.substring(5);
-
-                if (cmd.equals("echo") ||
-                    cmd.equals("exit") ||
-                    cmd.equals("type")) {
-
-                    System.out.println(cmd + " is a shell builtin");
-                } else {
-                    System.out.println(cmd + ": not found");
-                }
-            }
-            else {
-                System.out.println(command + ": command not found");
-            }
+        if (!found) {
+            System.out.println(cmd + ": not found");
         }
     }
 }
